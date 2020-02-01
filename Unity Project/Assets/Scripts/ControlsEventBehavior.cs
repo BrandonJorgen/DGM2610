357077f495dd6e0bd0dca01dc0b2fd743602e0e0
Brandon <brandonjorgen@gmail.com>
2020-02-01 13:45:09 -0700

    Started Canteen Code

--- Unity Project/Assets/Scripts/ControlsEventBehavior.cs
+++ Unity Project/Assets/Scripts/ControlsEventBehavior.cs
@@ -1,35 +1,45 @@
 ﻿using UnityEngine;
-using UnityEngine.Events;
+ using UnityEngine.Events;
+ 
+ public class ControlsEventBehavior : MonoBehaviour
+ {
+     public UnityEvent spaceDown, leftControlDown, leftControlUp, leftShiftDown, leftShiftUp, eDown, eUp;
+ 
+     private void Update()
+     {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+             spaceDown.Invoke();
+         }
+ 
+         if (Input.GetKeyDown(KeyCode.LeftControl))
+         {
+             leftControlDown.Invoke();
+         }
+ 
+         if (Input.GetKeyUp(KeyCode.LeftControl))
+         {
+             leftControlUp.Invoke();
+         }
+ 
+         if (Input.GetKeyDown(KeyCode.LeftShift))
+         {
+             leftShiftDown.Invoke();
+         }
+         
+         if (Input.GetKeyUp(KeyCode.LeftShift))
+         {
+             leftShiftUp.Invoke();
+         }
 
-public class ControlsEventBehavior : MonoBehaviour
-{
-    public UnityEvent spaceDown, leftControlDown, leftControlUp, leftShiftDown, leftShiftUp;
+         if (Input.GetKeyDown(KeyCode.E))
+         {
+             eDown.Invoke();
+         }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            spaceDown.Invoke();
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftControl))
-        {
-            leftControlDown.Invoke();
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftControl))
-        {
-            leftControlUp.Invoke();
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            leftShiftDown.Invoke();
-        }
-        
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            leftShiftUp.Invoke();
-        }
-    }
-}
+         if (Input.GetKeyUp(KeyCode.E))
+         {
+             eUp.Invoke();
+         }
+     }
+ }
---
+++ Unity Project/Assets/Scripts/FloatData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+    public float data, dataCap;
+
+    public void AddToData(int value)
+    {
+        data += value;
+
+        if (data > dataCap && dataCap != 0)
+        {
+            data = dataCap;
+        } else if (data <= 0)
+        {
+            data = 0;
+        }
+    }
+}
