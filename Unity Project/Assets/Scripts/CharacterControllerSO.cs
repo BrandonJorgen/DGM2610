c328117d1a2165c70b263c6672b49da695082d8b
Brandon <brandonjorgen@gmail.com>
2020-02-01 09:03:36 -0700

    Movement Control Improvements
    Player faces direction its moving
    Added a dash

--- Unity Project/Assets/Scripts/CharacterControllerSO.cs
+++ Unity Project/Assets/Scripts/CharacterControllerSO.cs
@@ -3,17 +3,22 @@
 [CreateAssetMenu]
 public class CharacterControllerSO : ScriptableObject
 {
-    private Vector3 position;
+    public Vector3 position;
 
-    public float moveSpeed = 5f, jumpSpeed = 10f, gravity = 9.81f;
-
+    public float moveSpeed = 5f, jumpSpeed = 10f, rollDistance = 100f, gravity = 9.81f;
+    
     public void MoveCharacter(CharacterController controller)
     {
         if (controller.isGrounded)
         {
             position.Set(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+            
+            if (position != Vector3.zero)
+            {
+                controller.transform.forward = position;
+            }
         }
-
+        
         position.y -= gravity * Time.deltaTime;
         controller.Move(position * Time.deltaTime);
     }
@@ -23,6 +28,18 @@ public class CharacterControllerSO : ScriptableObject
         if (controller.isGrounded)
         {
             position.y = jumpSpeed;
+        }
+        
+        controller.Move(position * Time.deltaTime);
+    }
+
+    public void DodgeRoll(CharacterController controller)
+    {
+        if (controller.isGrounded)
+        {
+            //Move forward by a specific amount at a specific speed
+            position.Set(Input.GetAxis("Horizontal") * rollDistance, 0, Input.GetAxis("Vertical") * rollDistance);
+            //Reduce controller height
         }
         
         controller.Move(position * Time.deltaTime);
