82b4bc3613605887f71162804a7241ebe4541620
Brandon <brandonjorgen@gmail.com>
2020-02-15 13:37:44 -0700

    Treasure Distraction mechanic start

---
+++ Unity Project/Assets/Scripts/InstantiateObjSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class InstantiateObjSO : ScriptableObject
+{
+    public GameObject objToInstantiate;
+
+    public void InstantiateObj(Transform location)
+    {
+        Instantiate(objToInstantiate, location.position, location.rotation);
+    }
+}
---
+++ Unity Project/Assets/Scripts/ObjPileBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPileBehavior : MonoBehaviour
+{
+    public float value;
+    private MeshFilter currentMesh;
+    public List<Mesh> meshList;
+    
+    private void Start()
+    {
+        currentMesh = GetComponent<MeshFilter>();
+    }
+
+    public void AddToPile(FloatDataSO dataObj)
+    {
+        value += dataObj.value;
+        //if statements checking value and assigning what mesh to display
+    }
+}
