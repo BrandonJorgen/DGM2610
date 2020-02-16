6aeb4e20a756697618f98500c9cfd6805a55702c
Brandon <brandonjorgen@gmail.com>
2020-02-16 10:09:54 -0700

    Fixed being able to add to treasure piles despite having no treasure

--- Unity Project/Assets/Scripts/BoolCheckBehavior.cs
+++ Unity Project/Assets/Scripts/BoolCheckBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine.Events;
 public class BoolCheckBehavior : MonoBehaviour
 {
     public BoolDataSO boolOne, boolTwo;
-    public bool bothTrue, oneTrueTwoFalse, oneFalseTwoTrue, bothFalse;
+    public bool bothTrue, oneTrueTwoFalse, oneFalseTwoTrue, bothFalse, oneTrue, oneFalse;
     public UnityEvent conditionsMet;
 
     public void TwoBoolCheck()
@@ -39,6 +39,19 @@ public class BoolCheckBehavior : MonoBehaviour
             {
                 conditionsMet.Invoke();
             }
+        }
+    }
+
+    public void OneBoolCheck()
+    {
+        if (oneTrue && boolOne.boolData)
+        {
+            conditionsMet.Invoke();
+        }
+
+        if (oneFalse && !boolOne.boolData)
+        {
+            conditionsMet.Invoke();
         }
     }
 }
---
+++ Unity Project/Assets/Scripts/ValueCheckSO.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ValueCheckSO : ScriptableObject
+{
+    public FloatDataSO dataObj;
+    public bool greaterThan, lessThan, equal;
+    public UnityEvent greaterThanEvent, lessThanEvent, equalEvent;
+
+    public void CheckAgainstFloat(float comparedFloat)
+    {
+        if (dataObj.value > comparedFloat && greaterThan)
+        {
+            greaterThanEvent.Invoke();
+        }
+        
+        if (dataObj.value < comparedFloat && lessThan)
+        {
+            lessThanEvent.Invoke();
+        }
+
+        if (dataObj.value == comparedFloat && equal)
+        {
+            equalEvent.Invoke();
+        }
+    }
+}
