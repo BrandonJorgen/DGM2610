a3fb6565142b273eb24817a35c5fc51170fa2d22
Brandon <brandonjorgen@gmail.com>
2020-03-11 11:05:31 -0600

    Initial enemy damage knockback code

---
+++ Unity Project/Assets/Scripts/TransformBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class TransformBehavior : MonoBehaviour
+{
+    private Transform transformObj;
+
+    public float knockbackFloat;
+
+    private void Start()
+    {
+        transformObj = GetComponent<Transform>();
+    }
+
+    public void Knockback()
+    {
+        transformObj.position += transformObj.TransformDirection(Vector3.back) * knockbackFloat;
+    }
+}
