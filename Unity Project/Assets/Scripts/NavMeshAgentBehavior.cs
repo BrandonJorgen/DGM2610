8c28cf1f93f9c4dfe39a3961233502ea99e0f540
Brandon <brandonjorgen@gmail.com>
2020-03-23 13:53:46 -0600

    Enemies now return to spawn point after losing the player for a second
    Once the player leaves the enemy's sight, it goes to last known position, sits for a second, then returns to its spawn location

--- Unity Project/Assets/Scripts/NavMeshAgentBehavior.cs
+++ Unity Project/Assets/Scripts/NavMeshAgentBehavior.cs
@@ -9,21 +9,32 @@ public class NavMeshAgentBehavior : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Vector3 spawnLoc;
+    private bool hadTarget;
     
     public AITargetingBehavior targetingBehavior;
-    public float stunnedTimer = 1f, rotateSpeed = 1f;
+    public float stunnedTimer = 1f, returnTimer = 1f, rotateSpeed = 1f;
     public UnityEvent endOfStunCountdown;
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        spawnLoc = agent.transform.position;
+        spawnLoc = transform.position;
+    }
+
+    private void Update()
+    {
+        SetDestination();
+        if (hadTarget && targetingBehavior.possibleTargetList.Count == 0)
+        {
+            LostSight();
+        }
     }
 
     public void SetDestination()
     {
         if (targetingBehavior.possibleTargetList.Count != 0)
         {
+            hadTarget = true;
             agent.destination = targetingBehavior.possibleTargetList[0].gameObj.transform.position;
             var targetRotation = Quaternion.LookRotation(targetingBehavior.possibleTargetList[0].gameObj.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
@@ -32,6 +43,15 @@ public class NavMeshAgentBehavior : MonoBehaviour
         if (targetingBehavior.possibleTargetList.Count != 0 && agent.remainingDistance <= agent.stoppingDistance)
         {
             //make the agent back away
+        }
+    }
+
+    public void LostSight()
+    {
+        if (agent.remainingDistance <= 0.25f + agent.stoppingDistance)
+        {
+            hadTarget = false;
+            StartCoroutine(ReturnToSpawnCountdown());
         }
     }
 
@@ -51,5 +71,12 @@ public class NavMeshAgentBehavior : MonoBehaviour
         yield return new WaitForSeconds(stunnedTimer);
         SetDestination();
         endOfStunCountdown.Invoke();
+    }
+
+    private IEnumerator ReturnToSpawnCountdown()
+    {
+        Debug.Log("return to spawn countdown started");
+        yield return new WaitForSeconds(returnTimer);
+        ReturnToSpawn();
     }
 }
