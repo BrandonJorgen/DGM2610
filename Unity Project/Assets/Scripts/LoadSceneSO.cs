1c7bc6e8772136efbbdb476f4750582a033767b9
Brandon <brandonjorgen@gmail.com>
2020-03-25 12:07:38 -0600

    Game is beatable and metrics have been changed
    Player move speed: 7 => 8
    Rotter move speed: 3.5 => 4.5
    Rotter health: 2 => 6
    Behemoth health: 4 => 8

---
+++ Unity Project/Assets/Scripts/LoadSceneSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class LoadSceneSO : ScriptableObject
+{
+    public void LoadCurrentScene()
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+}
